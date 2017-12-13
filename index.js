f05d169448a71f4d8a5405800ed34b6b269d7683
rwenite <rwenite@gmail.com>
2017-12-12 21:46:01 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// Richard Wen
+// rrwen.dev@gmail.com
+
+/**
+ * Description.
+ *
+ * @module twitter2mongodb-cli
+ * @param {Object} [options={}] parameter description.
+ * @returns {Object} return description.
+ *
+ * @example
+ * var twitter2mongodbcli = require('twitter2mongodb-cli');
+ */
+module.exports = options => {
+	options = options || {};
+};
